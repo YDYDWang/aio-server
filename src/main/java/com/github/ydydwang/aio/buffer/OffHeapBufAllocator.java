70d00610a887837e658abf141218754d8e7fc0f9
user <user@192.168.100.114>
2017-11-21 17:16:09 +0800

    1. Remove remain code 2. Remove sample code

--- src/main/java/com/github/ydydwang/aio/buffer/OffHeapBufAllocator.java
+++
@@ -1,8 +0,0 @@
-package com.github.ydydwang.aio.buffer;
-
-public class OffHeapBufAllocator implements BufAllocator {
-
-	public Buf allocate(int capacity) {
-		return new OffHeapBuf(capacity);
-	}
-}
